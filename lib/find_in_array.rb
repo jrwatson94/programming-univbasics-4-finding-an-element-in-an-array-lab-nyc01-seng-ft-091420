93ebf5bde2495b20add606492a2b0351cd1776a7
Reid Watson <j.watsonreid@gmail.com>
2020-09-01 16:40:44 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   
   while array[counter] do
     if array[counter] == value_to_find
-      return array.index()
+      return array[counter].index()
     else
       return nil
     end
